07b49fbf5934cc594e922d5520fc3990c38b6b5d
Kyle Deppe <krd4h8@mail.missouri.edu>
2017-12-03 20:00:30 -0600

    Added a comment

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+  // This program was designed to print "Hello, world!". Its revolutionary design is customized
+  // to give the best greeting experience possible to any user who runs this in a terminal.
   print("Hello, world!\n");
 //
 ?>
